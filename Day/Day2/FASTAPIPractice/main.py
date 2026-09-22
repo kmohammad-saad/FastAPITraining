8cf9a97d9f382630d344aeca2d0383308dcc1e4b
kmohammad-saad <kmohammadsaad116@gmail.com>
2026-09-22 13:31:17 +0530

    #Path parameter with type hint

--- Day/Day2/FASTAPIPractice/main.py
+++ Day/Day2/FASTAPIPractice/main.py
@@ -9,8 +9,18 @@ def about():
 
 @app.get("/health")
 def health():
-    return{"status": "ok"}#adding 
-#POST requuest
+    return{"status": "ok"}#adding
+
+#Post request
 @app.post("/create")
 def create_something():
-    return{"message": "something created"}
+    return {"message":"Created"}
+
+#Path parameter with type hint
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","usn":usn}
+
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno):
+    return {"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
